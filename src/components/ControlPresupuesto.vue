49c13c3f5bfa844232a0cc2c3417e700a5508c2b
mariohood <manhattan199@hotmail.com>
2025-02-19 10:02:19 -0300

    147 Graficar e Mostrar porcentagem de gasto

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,5 +1,7 @@
 <script setup>
-  import imagen from '../assets/img/grafico.jpg'
+  import { computed } from 'vue'
+  import CircleProgress from 'vue3-circle-progress'
+  import "vue3-circle-progress/dist/circle-progress.css"
   import {formatearCantidad} from '../helpers'
 
   defineEmits(['reset-app'])
@@ -17,15 +19,26 @@
       type: Number,
       required: true
     }
+  })
 
+  const porcentaje = computed(() => {
+    return parseInt(((props.presupuesto - props.disponible) / props.presupuesto) * 100)
   })
 </script>
 
 <template>
   <div class="dos-columnas">
     <div class="contenedor-grafico">
-      <img
-        :src="imagen"
+
+      <p class="porcentaje">{{ porcentaje }}%</p>
+
+      <CircleProgress
+        :percent="porcentaje"
+        :size="250"
+        :borderWidth="30"
+        :border-bg-width="30"
+        fill-color="#3b82f6"
+        empty-color="#e1e1e1"     
       />
 
     </div>
@@ -60,6 +73,23 @@
 
 <style scoped>
 
+  .contenedor-grafico {
+    position: relative;
+  }
+
+  .porcentaje {
+    position: absolute;
+    margin:auto;
+    top:calc(50% - 1.5rem);
+    left: 0;
+    right: 0;
+    text-align: center;
+    z-index: 100;
+    font-size: 3rem;
+    font-weight: 900;
+    color: var(--gris-oscuro);
+
+  }
   .dos-columnas {
     display: flex;
     flex-direction: column;
